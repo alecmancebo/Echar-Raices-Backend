87c139b585a8baad21f4760f705308bd15e8b0b8
Alec <alek.rmancebo@gmail.com>
2026-07-16 00:50:42 +0200

    env

--- db.js
+++ db.js
@@ -1,14 +1,12 @@
 import dotenv from "dotenv";
 import { MongoClient } from "mongodb";
 
-// CARGA DE VARIABLES DE ENTORNO
 dotenv.config();
 
-// CONFIGURACION DE CONEXION
 const urlmongo = process.env.MONGO_URL;
 let conexion;
 
-// CONEXION UNICA A MONGODB
+// CONEXION A MONGODB
 export async function conectarDB() {
     if (conexion) return conexion;
     
